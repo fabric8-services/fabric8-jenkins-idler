26f4196e0cc67c191ef825818334364bd3f86059
Václav Pavlín <vaclav.pavlin@gmail.com>
2017-10-17 19:46:19 +0200

    Add missing file

---
+++ openshiftcontroller/project.go
@@ -0,0 +1,85 @@
+package openshiftcontroller
+
+import (
+	"encoding/json"
+	"strings"
+	"errors"
+	"sort"
+
+	log "github.com/sirupsen/logrus"
+)
+
+type Projects struct {
+	Items []*Project `json:"items"`
+}
+
+type Project struct {
+	Metadata Metadata `json:"metadata"`
+}
+
+func ProcessProjects(data []byte) []string {
+	tmp := Projects{}
+	result := make([]string, 0)
+	err := json.Unmarshal(data, &tmp)
+	if err != nil {
+		log.Panic(err)
+	}
+
+	for _, proj := range tmp.Items {
+		if strings.HasSuffix(proj.Metadata.Name, "-jenkins") {
+			result = append(result, proj.Metadata.Name[:len(proj.Metadata.Name)-8])
+		}
+	}
+
+	return result
+}
+
+func AppendProject(groups []*[]string, project string) ([]*[]string, error) {
+	s := 0
+	if len(groups) == 0 {
+		return groups, errors.New("Cannot append project to empty groups.")
+	}
+
+	if len(project) == 0 {
+		return groups, nil
+	}
+	l := len(*groups[0])
+	for i, g := range groups {
+		if len(*g) < l {
+			l = len(*g)
+			s = i
+		}
+	}
+
+	g := append(*groups[s], project)
+	groups[s] = &g
+
+	return groups, nil
+}
+
+func UpdateProjects(groups []*[]string, projects []string) ([]*[]string, error) {
+	if len(groups) == 0 {
+		return groups, errors.New("Cannot append project to empty groups.")
+	}
+
+	if len(projects) == 0 {
+		return groups, nil
+	}
+
+	for _, g := range groups {
+		for _, p := range *g {
+			e := sort.SearchStrings(projects, p)
+			if e < len(projects) && projects[e] == p {
+				projects = append(projects[:e], projects[e+1:]...)
+			} 
+		}
+	}
+
+	for _, p := range projects {
+		groups, err := AppendProject(groups, p)
+		if err != nil {
+			return groups, err
+		}
+	}
+	return groups, nil
+}
