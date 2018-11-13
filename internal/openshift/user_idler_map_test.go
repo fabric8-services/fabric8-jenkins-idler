4336ddb89e625cc82c5940f56179b6d96c43c415
Khurram Baig <kbaig@redhat.com>
2018-11-13 16:07:54 +0530

    Added Multiple Goroutine ReadWrite Test for useridler map

--- internal/openshift/user_idler_map_test.go
+++ internal/openshift/user_idler_map_test.go
@@ -58,21 +58,27 @@ func TestUserIdlerMap_StoreDelete(t *testing.T) {
 	uidlerA := &idler.UserIdler{}
 	uidlerB := &idler.UserIdler{}
 	uidlerC := &idler.UserIdler{}
-	m.Store("foo_a", uidlerA)
-	result, ok := m.Load("foo_a")
-	assert.True(t, ok, "There should be an entry mapped")
-	assert.Equal(t, uidlerA, result, "result retrieved should be same")
-	m.Store("foo_b", uidlerB)
-	result, ok = m.Load("foo_b")
-	assert.True(t, ok, "There should be an entry mapped")
-	assert.Equal(t, uidlerB, result, "result retrieved should be same")
-	m.Store("foo_c", uidlerC)
-	result, ok = m.Load("foo_c")
-	assert.True(t, ok, "There should be an entry mapped")
-	assert.Equal(t, uidlerC, result, "result retrieved should be same")
-	m.Delete("foo_a")
-	idler, ok := m.Load("foo_a")
-	assert.False(t, ok, "There should be no entry mapped")
-	assert.Nil(t, idler, "No reference should be returned")
+	go func() {
+		m.Store("foo_a", uidlerA)
+		result, ok := m.Load("foo_a")
+		assert.True(t, ok, "There should be an entry mapped")
+		assert.Equal(t, uidlerA, result, "result retrieved should be same")
+		m.Delete("foo_a")
+		idler, ok := m.Load("foo_a")
+		assert.False(t, ok, "There should be no entry mapped")
+		assert.Nil(t, idler, "No reference should be returned")
 
+	}()
+	go func() {
+		m.Store("foo_b", uidlerB)
+		result, ok := m.Load("foo_b")
+		assert.True(t, ok, "There should be an entry mapped")
+		assert.Equal(t, uidlerB, result, "result retrieved should be same")
+	}()
+	go func() {
+		m.Store("foo_c", uidlerC)
+		result, ok := m.Load("foo_c")
+		assert.True(t, ok, "There should be an entry mapped")
+		assert.Equal(t, uidlerC, result, "result retrieved should be same")
+	}()
 }
